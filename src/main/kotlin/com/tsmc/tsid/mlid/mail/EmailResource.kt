60367763463ed9ab14fef8dcf500a9a3422190e3
Brandy <dickgg@gmail.com>
2024-05-27 23:32:32 +0800

    dynamic sender

--- src/main/kotlin/com/tsmc/tsid/mlid/mail/EmailResource.kt
+++ src/main/kotlin/com/tsmc/tsid/mlid/mail/EmailResource.kt
@@ -16,19 +16,22 @@ import jakarta.ws.rs.core.Response
 class EmailResource(val mailClient: MailClient) {
     @POST
     suspend fun sendEmail(payload: EmailPayload): Response {
-        if(payload.body.isEmpty()){
-            throw RuntimeException("Should have one of text or html content")
-        }
-        if(payload.recipient.isEmpty()){
-            throw RuntimeException("Should have receiver")
-        }
+        if (payload.body.isEmpty())
+            {
+                throw RuntimeException("Should have one of text or html content")
+            }
+        if (payload.recipient.isEmpty())
+            {
+                throw RuntimeException("Should have receiver")
+            }
         MailMessage().apply {
             subject = payload.subject
             from = payload.from
             to = payload.recipient
-            if("html"==payload.contentType.lowercase()){
-                html = payload.body
-            }else{
+            if ("html" == payload.contentType.lowercase())
+                {
+                    html = payload.body
+                } else {
                 text = payload.body
             }
         }.let { message ->
--- src/test/kotlin/com/tsmc/tsid/mlid/EmailResourceTest.kt
+++ src/test/kotlin/com/tsmc/tsid/mlid/EmailResourceTest.kt
@@ -2,25 +2,22 @@ package com.tsmc.tsid.mlid
 
 import com.tsmc.tsid.mlid.mail.EmailPayload
 import io.quarkus.test.junit.QuarkusTest
-
-import org.junit.jupiter.api.Test
-import javax.inject.Inject
-import io.quarkus.mailer.MockMailbox
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
+import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class EmailResourceTest {
-
     @Test
     fun testSendEmailEndpoint() {
-        val emailPayload = EmailPayload(
-            recipient = listOf("recipient@example.com"),
-            subject = "Test Email",
-            body = "This is a test email.",
-            contentType = "text",
-            from = "brandy@txmx.com"
-        )
+        val emailPayload =
+            EmailPayload(
+                recipient = listOf("recipient@example.com"),
+                subject = "Test Email",
+                body = "This is a test email.",
+                contentType = "text",
+                from = "brandy@txmx.com",
+            )
 
         RestAssured.given()
             .contentType(ContentType.JSON)
@@ -31,11 +28,11 @@ class EmailResourceTest {
             .statusCode(200)
 
         EmailPayload(
-            recipient = listOf("recipient@example.com","abad@ggga.dcd"),
+            recipient = listOf("recipient@example.com", "abad@ggga.dcd"),
             subject = "Test Email 2 ",
             body = "This is a test email.",
             contentType = "html",
-            from = "brandy@txmx.com"
+            from = "brandy@txmx.com",
         ).let {
             RestAssured.given()
                 .contentType(ContentType.JSON)
